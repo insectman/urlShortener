ea73dbf6c1ad9b5e593b63b6481cd72edccb9e73
insectman <insectman@yandex.ru>
2017-09-20 21:41:05 +0300

    add custom config + url pair helper

---
+++ config/custom.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+
+	urlpair : {
+
+		//---------------days-hrs--mins-scn--ms--
+		storeTimeMS : 15 * 24 * 60 * 60 * 1000
+
+	}
+
+}
---
+++ providers/UrlpairHelper.js
@@ -0,0 +1,9 @@
+const UrlpairHelper = exports = module.exports = {}
+
+UrlpairHelper.delayedDeletion = function(urlpairId) {
+
+	//Config.get('custom.urlpair.storeTimeMS')
+
+	return;
+
+}
