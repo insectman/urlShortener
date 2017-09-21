94ae8457a17266976f95ebca5e2262dc607392da
insectman <insectman@yandex.ru>
2017-09-21 14:59:59 +0300

    add uglification

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gp_uglify = require('gulp-uglify');
 
 gulp.task('build', function () {
     return browserify('./resources/scripts/main.jsx')
     	.transform(babelify, {presets: ["es2015", "react"]})
         .bundle()
         .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(gp_uglify())
         .pipe(gulp.dest('./public/assets/js/'));
 });
 
