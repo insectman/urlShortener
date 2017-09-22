5dcac31d8533cdf56ef25a6218e59e1cb0c58c53
insectman <insectman@yandex.ru>
2017-09-22 15:31:22 +0300

    create first form component

--- resources/scripts/main.jsx
+++ resources/scripts/main.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+class ShortUrlForm extends React.Component {
+	
+	render() {
+		return <form>
+			<li className="form-row">
+				<label htmlFor = "shortUrl">Shorten your URL</label>
+				<input name = "shortUrl" id = "shortUrl" placeholder="Type/paste your URL here"></input>
+			</li>
+			<li className="form-row">
+				<button type="submit">Submit</button>
+			</li>
+		</form>
+	}
+
+}
+
 ReactDOM.render(
 	<div className="main-content">
-		<h1>Hello, world!</h1>
+		<ShortUrlForm></ShortUrlForm>
 	</div>,
   document.getElementById('root')
 );
